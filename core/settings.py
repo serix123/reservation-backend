e32c17ba7c143126b4c7b65d392f39ea24e99a48
Jake Suerte <jakeandrey.suerte@awsys-i.com>
2025-04-09 18:22:50 +0800

    for deployment (test)

--- core/settings.py
+++ core/settings.py
@@ -21,16 +21,16 @@ load_dotenv()  # Load environment variables from .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(
-    BASE_DIR, "staticfiles"
-)  # this is where collectstatic will gather all static files.
+STATIC_URL = "static/"
+# STATIC_ROOT = os.path.join(
+#     BASE_DIR, "staticfiles"
+# )  # this is where collectstatic will gather all static files.
 
-STATICFILES_DIRS = [
-    os.path.join(
-        BASE_DIR, "static"
-    ),  # folders where you have static files in development.
-]
+# STATICFILES_DIRS = [
+#     os.path.join(
+#         BASE_DIR, "static"
+#     ),  # folders where you have static files in development.
+# ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -193,7 +193,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
